46c19c2a6480df004495444cb6d5476377edaaa8
prithiveram <prithive.chandrasekaran@gmail.com>
2024-12-08 20:14:43 +0530

    user

--- controllers/User.js
+++ controllers/User.js
@@ -20,7 +20,7 @@ const login = async (req, res) => {
 
             const filteredCustomers = await sendLastDateNotification(req, res);
 
-            res.status(200).json({token, filteredCustomers});
+            res.status(200).json({token, user, filteredCustomers});
         });
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
@@ -87,19 +87,73 @@ const getUsersByNameOrMobileNumber = (req, res) => {
     });
 };
 
+// const updateUserById = async (req, res) => {
+//     console.log("inside", req);
+//     const id = req.query.id;
+//     const { username, email, password, is_admin, date_of_birth, marriage_date, address, mobilenumber } = req.body;
+//     const password_hash = await bcrypt.hash(password, 10);
+//     const updatedUserData = { username, email, password_hash, is_admin, date_of_birth, marriage_date, address, mobilenumber};
+//     User.updateUserById(id, updatedUserData, (err, result) => {
+//         if (err) {
+//             return res.status(500).json({ error: err.message });
+//         }
+//         res.status(200).json(result);
+//     });
+// };
+
+
+
 const updateUserById = async (req, res) => {
-    console.log("inside", req);
-    const id = req.query.id;
-    const { username, email, password, is_admin, date_of_birth, marriage_date, address, mobilenumber } = req.body;
-    const password_hash = await bcrypt.hash(password, 10);
-    const updatedUserData = { username, email, password_hash, is_admin, date_of_birth, marriage_date, address, mobilenumber};
-    User.updateUserById(id, updatedUserData, (err, result) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.status(200).json(result);
-    });
+    try {
+        
+        const { username, email, password, is_admin, date_of_birth, marriage_date, address, mobilenumber, old_password } = req.body;
+
+        User.findByUsernameOrMobileAndPassword(username, password, async (err, user) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            if (!user) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+
+            const passwordMatch = await bcrypt.compare(old_password, user.password_hash);
+            if (!passwordMatch) {
+                return res.status(400).json({ error: 'Old password does not match' });
+            }
+
+            let password_hash = user.password_hash;
+            if (password) {
+                password_hash = await bcrypt.hash(password, 10);
+            }
+
+            const updatedUserData = {
+                username: username || user.username,
+                email: email || user.email,
+                password_hash: password_hash,
+                is_admin: is_admin !== undefined ? is_admin : user.is_admin,
+                date_of_birth: date_of_birth || user.date_of_birth,
+                marriage_date: marriage_date || user.marriage_date,
+                address: address || user.address,
+                mobilenumber: mobilenumber || user.mobilenumber
+            };
+
+            User.updateUserById(id, updatedUserData, (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: err.message });
+                }
+                res.status(200).json(result);
+            });
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
+
+
+
+
+
+
 
 const deleteUserById = async (req, res) => {
     try {
@@ -165,5 +219,5 @@ module.exports = {
     getAllUsers,
     getUsersByNameOrMobileNumber,
     deleteUserById,
-    updateUserById,
+    updateUserById
 }
